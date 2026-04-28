26c2bc909125c701f2bd1c829d0bb13d7996f237
polo910 <mikolaj.polok19@gmail.com>
2026-04-28 13:49:55 +0200

    lab6 begining

--- federation/test_single_provider.py
+++
@@ -1,6 +0,0 @@
-import requests
-response = requests.get("http://127.0.0.1:8001/observations")
-data = response.json()
-print("NUMBER OF OBSERVATIONS:", len(data))
-print("FIRST RECORD:")
-print(data[0])
--- federation/test_single_provider2.py
+++
@@ -1,7 +0,0 @@
-import requests
-object_id = "OBJ-003"
-response = requests.get(f"http://127.0.0.1:8001/observations/{object_id}")
-data = response.json()
-print("OBJECT:", object_id)
-print("RESULTS:", len(data))
-print(data)
